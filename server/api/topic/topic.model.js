f6a7da5cbe6005ee5d1dc6a98b63a93c5a583d10
Scott Martineau <winteralce@gmail.com>
2016-04-15 11:33:24 -0600

    set up the schema and the functions for the endpoints

--- server/api/topic/topic.model.js
+++ server/api/topic/topic.model.js
@@ -2,10 +2,13 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var TopicSchema = new Schema({
-  title: String,
-  parentNodes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Topics'}],
-  childrenNodes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Topics'}],
-  references: [String] //a string of links
+  title: {type: String, required: true},
+  parentNodes: [{type: Schema.Types.ObjectId, ref: 'Topics'}],
+  childrenNodes: [{type: Schema.Types.ObjectId, ref: 'Topics'}],
+  references: [{
+      name: {type: String},
+      link: {type: String}
+  }] //a string of links
 });
 
 module.exports = mongoose.model('Topics', TopicSchema);
